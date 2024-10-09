0e9b90f323baa8fe01b89ce78c3103d99dc46b98
DerrekNguyen <mnguye21@kent.edu>
2024-10-09 00:04:13 -0400

    Started on day buttons logic and style

--- MVVM/ViewModel/AddViewModel.cs
+++ MVVM/ViewModel/AddViewModel.cs
@@ -2,15 +2,28 @@
 using Derrek_Application.MVVM.Model;
 using System;
 using System.Collections.Generic;
+using Derrek_Application.MVVM.View;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Forms;
+using System.Windows.Markup;
+using System.Linq.Expressions;
 
 namespace Derrek_Application.MVVM.ViewModel
 {
    class AddViewModel : ObservableObject
    {
+
       public RelayCommand SubmitAssignmentCommand { get; set; }
+      public RelayCommand DayButtonCommand { get; set; }
+
+      private Style _unchecked = new Style(typeof(System.Windows.Controls.RadioButton), (Style)System.Windows.Application.Current.Resources["UnselectedDayButtonTheme"]);
+      private Style _checked = new Style(typeof(System.Windows.Controls.RadioButton), (Style)System.Windows.Application.Current.Resources["SelectedDayButtonTheme"]);
+      private Dictionary<string, bool> DaysSelected = new Dictionary<string, bool>();
 
       private string _name;
       public string Name
@@ -43,6 +56,31 @@ namespace Derrek_Application.MVVM.ViewModel
       private bool CheckValid()
       {
          return Description != null && Name != null;
+      }
+
+      private Style _buttonStyle;
+      public Style ButtonStyle
+      {
+         get
+         {
+            return _buttonStyle;
+         }
+         set
+         {
+            _buttonStyle = value;
+            OnPropertyChanged(nameof(ButtonStyle));
+         }
+      }
+
+      private void InitializeDaySelected()
+      {
+         DaysSelected.Add("MondayButton", false);
+         DaysSelected.Add("TuesdayButton", false);
+         DaysSelected.Add("WednesdayButton", false);
+         DaysSelected.Add("ThursdayButton", false);
+         DaysSelected.Add("FridayButton", false);
+         DaysSelected.Add("SaturdayButton", false);
+         DaysSelected.Add("SundayButton", false);
       }
 
       public AddViewModel(AssignmentListViewModel assignmentList, MainViewModel mainView)
